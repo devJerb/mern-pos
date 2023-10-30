5832028542022d3d0ac6bda26de5d5444705ed02
devJerb <gutierrez.jeromebryant@gmail.com>
2023-10-31 01:13:23 +0800

    updated mongodb conn

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const { MONGO_URL, PORT } = process.env;
+
+const PORT = process.env.PORT || 8080;
 
 require("dotenv").config();
 
 mongoose
-  .connect(MONGO_URL, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -20,7 +21,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: [`http://localhost:${POST}`],
+    origin: [`http://localhost:${PORT}`],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
