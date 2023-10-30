2cd5b72f8cb0902d25d0911064e1c2c5a9c32bc0
devJerb <gutierrez.jeromebryant@gmail.com>
2023-10-31 01:30:46 +0800

    added schema and jwt

--- server/models/model.js
+++ server/models/model.js
@@ -1,1 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Your email address is required"],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Your password is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const model = mongoose.model(
+  "User",
+  userSchema.pre("save", async () => {
+    this.password = await bcrypt.hash(this.password, 12);
+  })
+);
+
+export default model;
