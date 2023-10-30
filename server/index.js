b884398c74538fc8cda439c3ece7e8d3f6e663bb
devJerb <gutierrez.jeromebryant@gmail.com>
2023-10-31 01:01:01 +0800

    established mongodb conn

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const PORT = 8000;
+const { MONGO_URL, PORT } = process.env;
+
+require("dotenv").config();
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is connected!"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
-    console.log(`Server on port http://localhost:${PORT}/`);
+  console.log(`Server on port http://localhost:${PORT}/`);
 });
+
+app.use(
+  cors({
+    origin: [`http://localhost:${POST}`],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
